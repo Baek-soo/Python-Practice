7e57c3c535a9c8da7f733ded5e516a8cdf5be9da
BaekSoo <heech980120@gmail.com>
2021-08-03 17:03:52 +0900

    21.08.03

--- 2. Codingtest/Kakao Internship(2020) 1.py
+++ 2. Codingtest/Kakao Internship(2020) 1.py
@@ -1,39 +1,32 @@
-#key_pad left/right hand
-
 def solution(numbers, hand):
     answer = ''
-    
-    key_pad = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1,0), 5:(1,1), 6:(1,2), 7:(2,1), 8:(2,2), 9:(2,3), '*':(3,0), 0:(3,1), '#':(3,2)}
+    key_pad = {1:(0,0), 2:(0,1), 3:(0,2), 4:(1,0), 5:(1,1), 6:(1,2), 7:(2,0), 8:(2,1), 9:(2,2), '*':(3,0), 0:(3,1), '#':(3,2)}
+    left = [1,4,7]
+    right = [3,6,9]
 
-    left = [1, 4, 7]
-    right = [3, 6, 9]
     lhand = '*'
     rhand = '#'
 
-    for i in numbers: #1, 4, 7
+    for i in numbers:
         if i in left:
             answer += 'L'
             lhand = i
-        elif i in right: #3, 6, 9
+        elif i in right:
             answer += 'R'
             rhand = i
-        else: #2, 5, 8, 0
-            curpos = key_pad[i]
-            lpos = key_pad[lhand]
-            rpos = key_pad[rhand]
-            #오른손과 숫자의 거리
-            l_dist = abs(curpos[0] - lpos[0]) + abs(curpos[1] - lpos[1])
-            #왼손과 숫자의 거리
-            r_dist = abs(curpos[0] - rpos[0]) + abs(curpos[1] - rpos[1])
-            #왼손이 오른손보다 짧을 때
+        else:
+            toMove = key_pad[i]
+            l_pos = key_pad[lhand]
+            r_pos = key_pad[rhand]
+            l_dist = abs(toMove[0]-l_pos[0]) + abs(toMove[1]-l_pos[1])
+            r_dist = abs(toMove[0]-r_pos[0]) + abs(toMove[1]-r_pos[1])
+
             if l_dist < r_dist:
                 answer += 'L'
                 lhand = i
-            #오른손이 왼손보다 짧을 때
-            if r_dist < l_dist:
+            elif l_dist > r_dist:
                 answer += 'R'
                 rhand = i
-            #둘 사이의 거리 같을 경우 
             else:
                 if hand == 'left':
                     answer += 'L'
@@ -44,5 +37,3 @@ def solution(numbers, hand):
 
 
     return answer
-
-
