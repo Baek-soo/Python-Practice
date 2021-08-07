600000af1e117c45495d37a7ae8cae5c9567cd9b
BaekSoo <heech980120@gmail.com>
2021-08-07 15:35:13 +0900

    21.08.07

--- 2. Codingtest/Kakao internship(2021) 1.py
+++ 2. Codingtest/Kakao internship(2021) 1.py
@@ -1,18 +1,7 @@
 def solution(s):
-    answer = ''
-    numbers = {'zero':'0', 'one':(1,0), 'two':(2,0), 'three':(3,0), 'four':(4,0), 'five':(5,0), 'six':(6,0), 'seven':(7,0), 'eight':(8,0), 'nine':(9,0)}
-
-    for i in s:
-        if i in numbers:
-            answer += numbers[0]
-        else:
-            answer += i
-
-
-
-
-    return answer
-
-
-print(solution(123))
-#이거 풀어야함
+    numbers = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    
+    answer  = s
+    for key, value in numbers.items():
+        answer = answer.replace(key, value)
+    return int(answer)
