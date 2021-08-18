7744f00c31ccbcc7c564dcd94693fda698be6289
BaekSoo <heech980120@gmail.com>
2021-08-18 16:53:14 +0900

    21.08.18

--- 2. Codingtest/Heap more spicy.py
+++ 2. Codingtest/Heap more spicy.py
@@ -2,34 +2,37 @@
 # 스코빌 지수가 가장 낮은 두개의 음식을 섞는다
 # 섞은 음식 = 가장 맵지 않은 음식의 스코빌 지수 + (두번재로 맵지 않은 음식 스코빌 지수 * 2)
 # 스코빌 지수가 k 이상이 될 때 까지 반복한다. 
+
+import heapq
+
 def solution(scoville, k):
-    mixed = 0
     count = 0
-    if len(scoville) > 0:
-        while k > mixed:
-            scoville.sort()
-            least_1 = scoville[0]; least_2 = scoville[1]
-            mixed = least_1 + (least_2 * 2)
-            scoville.append(mixed)
-            scoville.remove(least_1); scoville.remove(least_2)
-            count += 1
-        return count
-    else: 
-        return -1
+    h = []
+    for i in scoville:
+        heapq.heappush(h, i)
+    while h[0] < k:
+        try:
+            heapq.heappush(h, heapq.heappop(h) + (heapq.heappop(h)*2))
+        except IndexError:
+            return -1
+        count += 1
+
+    return count
 
 
-# import heapq
+#미친 문제 풀이
+# import heapq as hq
 
-# def solution(scoville, k):
+# def solution(scoville, K):
+#     hq.heapify(scoville)
 #     count = 0
-#     h = []
-#     for i in scoville:
-#         heapq.heappush(h, i)
-#     while h[0] < k:
-#         try:
-#             heapq.heappush(h, heapq.heappop(h) + (heapq.heappop(h)*2))
-#         except IndexError:
+#     while True:
+#         first = hq.heappop(scoville)
+#         if first >= K:
+#             break
+#         if len(scoville) == 0:
 #             return -1
-#         count += 1
-
+#         second = hq.heappop(scoville)
+#         hq.heappush(scoville, first + second*2)
+#         count += 1  
 #     return count
