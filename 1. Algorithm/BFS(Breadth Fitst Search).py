26f76ce949d1320d4ff7d49c3d29525daf8ed098
BaekSoo <heech980120@gmail.com>
2021-08-30 19:59:12 +0900

    21.08.30

---
+++ 1. Algorithm/BFS(Breadth Fitst Search).py
@@ -0,0 +1,13 @@
+#한우물판 파서 탐색 <-> DFS
+from collections import deque
+
+def BFS(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            queue += graph[n] - set(visited)
+    return visited
